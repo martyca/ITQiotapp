c7eb8c0bb29dfc82cb6b45f43328bad4f8de5534
Martin Caarels <martin@caarels.com>
2018-10-21 11:42:15 +0200

    nodeport and envrc

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'assets')))
+app.get('/', (req, res) => res.sendFile(
+  path.join(__dirname, 'views/index.html')
+))
+
+app.listen(port, () => console.log('Example app listening on port: ' + port))
