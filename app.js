abc871ea39e8f8a5f3fa2df2f8c3b21e0c07038c
Martin Caarels <martin@caarels.com>
2018-10-23 16:26:12 +0200

    basic funcitonality

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const path = require('path')
 const app = express()
 const AWS = require('aws-sdk')
 const port = process.env.PORT || 3000
-
-var credentials = new AWS.SharedIniFileCredentials({profile: 'itqiot'});
-var region = 'eu-central-1'
-
-AWS.config.update({region: region});
+AWS.config.update({region: 'eu-central-1'});
 AWS.config.credentials = credentials;
+var credentials = new AWS.SharedIniFileCredentials({profile: 'itqiot'});
+ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+
+
+
 
 console.log(credentials);
 app.use(express.json())
@@ -19,8 +20,25 @@ app.get('/', (req, res) => {
   )
 })
 app.post('/api', (req, res) => {
+  var body = req.body
+  var params = {
+    TableName: 'iot',
+    Item: {
+      'pkey': {S: body['pkey']},
+      'name': {S: body['name']},
+      'score': {N: body['score']}
+    }
+  }
+  ddb.putItem(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+      res.send("ddb update Error")
+    } else {
+      console.log("Success", data);
+      res.send("ddb update Success")
+    }
+  })
   console.log(req.body)
-  res.send('api response')
 })
 
 app.listen(port, () => console.log('Example app listening on port: ' + port))
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,18 @@
+function generateRandomString() {
+  var text = ""
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  var length = 32
+  for (var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  return text
+}
 function updateSlider(slideAmount) {
   var xhttp = new XMLHttpRequest()
   var name = document.getElementById("name").value;
   if(!name){name = 'anonymous'}
   var voteResult = document.getElementById("result");
   var data = {
+    "pkey": document.cookie,
     "name": name,
     "score": slideAmount
   }
@@ -18,4 +27,7 @@ function updateSlider(slideAmount) {
   )
   xhttp.send(JSON.stringify(data))
   voteResult.innerHTML = slideAmount
+}
+if(!document.cookie){
+  document.cookie = generateRandomString()
 }
