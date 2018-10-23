b51eb84420dd14c15f90978bbcff10f36bc34741
Martin Caarels <martin@caarels.com>
2018-10-23 15:26:59 +0200

    now with post

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const AWS = require('aws-sdk')
 const port = process.env.PORT || 3000
 
+var credentials = new AWS.SharedIniFileCredentials({profile: 'itqiot'});
+var region = 'eu-central-1'
+
+AWS.config.update({region: region});
+AWS.config.credentials = credentials;
+
+console.log(credentials);
+app.use(express.json())
 app.use(express.static(path.join(__dirname, 'assets')))
 app.get('/', (req, res) => {
   res.sendFile(
     path.join(__dirname, 'views/index.html')
   )
-  console.log(req.ip);
 })
-app.post('/api', (req, res) => res.send('api response'))
+app.post('/api', (req, res) => {
+  console.log(req.body)
+  res.send('api response')
+})
 
 app.listen(port, () => console.log('Example app listening on port: ' + port))
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,21 @@
 function updateSlider(slideAmount) {
-    var name = document.getElementById("name").value;
-    if(!name){name = 'anonymous'}
-    var voteResult = document.getElementById("result");
-    voteResult.innerHTML = slideAmount
+  var xhttp = new XMLHttpRequest()
+  var name = document.getElementById("name").value;
+  if(!name){name = 'anonymous'}
+  var voteResult = document.getElementById("result");
+  var data = {
+    "name": name,
+    "score": slideAmount
+  }
+  console.log(JSON.stringify(data))
+  xhttp.open(
+    "POST",
+    "api"
+  )
+  xhttp.setRequestHeader(
+    "Content-type",
+    "application/json;charset=UTF-8"
+  )
+  xhttp.send(JSON.stringify(data))
+  voteResult.innerHTML = slideAmount
 }
